9abf24fece9a8a79391d75bd259baea4bbd249e0
Jules Druguet <jules.druguet@soluti.fr>
2019-06-10 13:52:16 +0200

    Affichage de la durée d'exécution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
         QAP qap = new QAP();
         QAPSolution positionsFinal;
         /* = qap.simulatedAnnealing(100,0.99);
@@ -8,7 +14,7 @@ public class Main {
         }
         System.out.println("Recuit simulé - Fitness finale : " + qap.calculateFitness(positionsFinal));
         */
-        positionsFinal = qap.tabuSearchBis(10, 50000000);
+        positionsFinal = qap.tabuSearchBis(10, 500);
 
         System.out.println();
         System.out.println("Positions finales : ");
@@ -16,5 +22,15 @@ public class Main {
             System.out.print(positionsFinal.get(i) + " ; ");
         }
         System.out.println("\nTabou - Fitness finale : " + qap.calculateFitness(positionsFinal));
+        long endTime = System.nanoTime();
+        long durationMs = (endTime - startTime) / 1000000;
+        if (durationMs < 1000) {
+            System.out.println("\nDurée d'exécution : " + durationMs + "ms");
+        } else {
+            Date d = new Date((durationMs / 1000) * 1000L);
+            SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+            String time = df.format(d);
+            System.out.println("Durée d'exécution : " + time);
+        }
     }
 }
